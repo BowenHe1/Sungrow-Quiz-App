2df98672524e091fb4ac86d907a7e62f552640bb
Bowen He <bowen.he@vanderbilt.edu>
2026-02-25 11:46:40 -0600

    fix the typo of Google sheet name for the SC5000UD-MV-P3 safety branch.

--- quiz_app.py
+++ quiz_app.py
@@ -77,7 +77,7 @@ def save_submission(candidate_info, score, max_score, answers_log):
     # Using open_by_key is safer/faster than opening by name
     try:
         sh = gc.open_by_key("18kGBJLPUu-VdQT4bRdME-X29kJjv7f5GDNKnAQ7dU2s")
-        worksheet = sh.worksheet("SC5000UD_MV_P3_CSP_Safety") # consistently gets the corresponding tab
+        worksheet = sh.worksheet("SC5000UD_MV_P3_Safety") # consistently gets the corresponding tab
     except Exception as e:
         st.error(f"Google Sheets Connection Error: {e}")
         st.stop()
